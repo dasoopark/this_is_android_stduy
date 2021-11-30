2fbc5a092c234d7871a7d2de0631194e0324b3fa
dasoopark <ektn0514@gmail.com>
2021-12-01 00:21:07 +0900

    리사이클러뷰
    리사이클러뷰

--- app/src/main/java/kr/co/korearental/containerrecyclerview/CustomAdapter.kt
+++ app/src/main/java/kr/co/korearental/containerrecyclerview/CustomAdapter.kt
@@ -1,4 +1,43 @@
 package kr.co.korearental.containerrecyclerview
 
-class CustomAdapter {
+import android.content.ClipData
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.korearental.containerrecyclerview.databinding.ItemRecyclerBinding
+import java.text.SimpleDateFormat
+
+class CustomAdapter : RecyclerView.Adapter<Holder>() {
+    var listData = mutableListOf<Memo>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+       val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val memo = listData.get(position)
+        holder.setMemo(memo) //따로 구현한 함수
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+
 }
+
+class Holder(val binding : ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root) //ItemRecycler => item_recycler 레이아웃 이름임
+
+{
+    fun setMemo(memo : Memo)
+    {
+     binding.textNo.text = "${memo.no}"
+     binding.textTitle.text = memo.title
+     var sdf = SimpleDateFormat("yyyy/MM/dd")
+     var formattedDate = sdf.format(memo.timestamp)
+     binding.textDate.text = formattedDate
+    }
+
+}
+
--- app/src/main/java/kr/co/korearental/containerrecyclerview/MainActivity.kt
+++ app/src/main/java/kr/co/korearental/containerrecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.korearental.containerrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.korearental.containerrecyclerview.databinding.ActivityMainBinding
 
 
@@ -11,7 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val data:MutableList<Memo> = loadDate()
+        var adapter = CustomAdapter()
+        adapter.listData = data
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
+
+
+
     fun loadDate() : MutableList<Memo>
     {
         val data : MutableList<Memo> = mutableListOf() //뮤터블 컬렉션 선언
@@ -23,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             var memo = Memo(no, title, date)
             data.add(memo)
         }
-        return data
+        return data;
     }
 }
 
