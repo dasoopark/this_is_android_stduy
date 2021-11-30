a681b8dc4194831d4ef2ee6501fd24360a937603
dasoopark <ektn0514@gmail.com>
2021-11-30 23:17:04 +0900

    .
    .

--- app/src/main/java/kr/co/korearental/containerrecyclerview/MainActivity.kt
+++ app/src/main/java/kr/co/korearental/containerrecyclerview/MainActivity.kt
@@ -2,10 +2,29 @@ package kr.co.korearental.containerrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.korearental.containerrecyclerview.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+    fun loadDate() : MutableList<Memo>
+    {
+        val data : MutableList<Memo> = mutableListOf() //뮤터블 컬렉션 선언
+
+        for (no in 1..100)
+        {
+            val title = "이것이 안드로이드다 ${no}"
+            val date = System.currentTimeMillis()
+            var memo = Memo(no, title, date)
+            data.add(memo)
+        }
+        return data
     }
 }
+
+
---
+++ app/src/main/java/kr/co/korearental/containerrecyclerview/Memo.kt
@@ -0,0 +1,3 @@
+package kr.co.korearental.containerrecyclerview
+
+data class  Memo(var no: Int, var title: String, var timestamp: Long)
