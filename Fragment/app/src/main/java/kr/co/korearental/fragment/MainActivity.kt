16e665b8c390a4922abbece46766480683d08dab
dasoopark <ektn0514@gmail.com>
2021-12-02 00:05:39 +0900

    .
    .

--- Fragment/app/src/main/java/kr/co/korearental/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/kr/co/korearental/fragment/MainActivity.kt
@@ -2,10 +2,47 @@ package kr.co.korearental.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.korearental.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setFragment()
+
+        binding.btnSend.setOnClickListener {
+            listFragment.setValue("전달할 값")
+        }
+    }
+
+    lateinit var listFragment:ListFragment
+
+    fun setFragment() {
+        listFragment = ListFragment()
+
+        var bundle = Bundle()
+        bundle.putString("key1", "List Fragment")
+        bundle.putInt("key2", 20170727)
+        listFragment.arguments = bundle
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
+    }
+
+    fun goDetail(){
+        val detailFragment = DetailFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, detailFragment)
+        transaction.addToBackStack("detail")
+        transaction.commit()
+    }
+
+    fun goBack(){
+        onBackPressed()
     }
 }
