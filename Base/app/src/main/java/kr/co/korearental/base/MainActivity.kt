5395f7ad8f944eaae52da5cf564b1c81f4cc7782
dasoopark <ektn0514@gmail.com>
2021-12-09 22:31:34 +0900

    .
    .

--- Base/app/src/main/java/kr/co/korearental/base/MainActivity.kt
+++ Base/app/src/main/java/kr/co/korearental/base/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.co.korearental.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.korearental.base.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- CamerAndGallery/app/src/main/java/kr/co/korearental/camerandgallery/MainActivity.kt
+++ CamerAndGallery/app/src/main/java/kr/co/korearental/camerandgallery/MainActivity.kt
@@ -1,25 +1,89 @@
 package kr.co.korearental.camerandgallery
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
 import kr.co.korearental.camerandgallery.databinding.ActivityMainBinding
 import java.io.File
+import java.util.jar.Manifest
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun permissionGranted(requestCode: Int) {
-        TODO("Not yet implemented")
+    var photoUri:Uri? = null
+
+    lateinit var cameraPermission:ActivityResultLauncher<String>
+    lateinit var storagePermission:ActivityResultLauncher<String>
+
+    lateinit var cameraLauncher:ActivityResultLauncher<Uri>
+    lateinit var galleryLauncher:ActivityResultLauncher<String>
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(isGranted) {
+                setViews()
+            } else {
+                Toast.makeText(baseContext, "외부저장소 권한을 승인해야 앱을 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+
+        cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(isGranted) {
+                openCamera()
+            } else {
+                Toast.makeText(baseContext, "카메라 권한을 승인해야 카메라를 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess  ->
+            if(isSuccess) { binding.imagePreview.setImageURI(photoUri) }
+        }
+
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            binding.imagePreview.setImageURI(uri)
+        }
+
+        storagePermission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 
-    override fun permissionDenied(requestCode: Int) {
-        TODO("Not yet implemented")
+    fun setViews() {
+        binding.buttonCamera.setOnClickListener {
+            cameraPermission.launch(android.Manifest.permission.CAMERA)
+        }
+        binding.buttonGallery.setOnClickListener {
+            openGallery()
+        }
     }
 
+    fun openCamera() {
+        val photoFile = File.createTempFile(
+            "IMG_",
+            ".jpg",
+            getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
 
+        photoUri = FileProvider.getUriForFile(
+            this,
+            "${packageName}.provider",
+            photoFile
+        )
+
+        cameraLauncher.launch(photoUri)
+    }
+
+    fun openGallery() {
+        galleryLauncher.launch("image/*")
+    }
 }
