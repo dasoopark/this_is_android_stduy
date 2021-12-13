4c3cb963e97dc1f3383585d28ab5bc3337760e0c
dasoopark <ektn0514@gmail.com>
2021-12-14 03:48:22 +0900

    .
    .

---
+++ ContentResolver/app/src/main/java/kr/co/korearental/contentresolver/MainActivity.kt
@@ -0,0 +1,66 @@
+package kr.co.korearental.contentresolver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import kr.co.korearental.contentresolver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var storagePermission: ActivityResultLauncher<String> // 바뀐코드 =>ActivityResultLauncher
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(isGranted) {
+                startProcess()
+            } else {
+                Toast.makeText(baseContext, "외부저장소 권한을 승인해야 앱을 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+
+    fun startProcess() {
+        val adapter = MusicRecyclerAdapter()
+        adapter.musicList.addAll(getMusicList())
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun getMusicList() : List<Music> {
+        // 1. 음원 정보 주소
+
+        val listUrl = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        // 2. 음원 정보 컬럼들
+        val proj = arrayOf(
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.DURATION
+        )
+        // 3. 컨텐트리졸버의 쿼리에 주소와 컬럼을 입력하면 커서형태로 반환받는다
+        val cursor = contentResolver.query(listUrl, proj, null, null, null)
+        val musicList = mutableListOf<Music>()
+        while (cursor?.moveToNext() == true) {
+            val id = cursor.getString(0)
+            val title = cursor.getString(1)
+            val artist = cursor.getString(2)
+            val albumId = cursor.getString(3)
+            val duration = cursor.getLong(4)
+
+            val music = Music(id, title, artist, albumId, duration)
+            musicList.add(music)
+        }
+        return musicList
+    }
+}
