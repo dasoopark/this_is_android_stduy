61ea63f60d695e543402abf810ad47f7915f999d
dasoopark <ektn0514@gmail.com>
2021-12-14 01:54:36 +0900

    .
    .

--- ForegroundService/app/src/main/java/kr/co/korearental/foregroundservice/MainActivity.kt
+++ ForegroundService/app/src/main/java/kr/co/korearental/foregroundservice/MainActivity.kt
@@ -1,11 +1,27 @@
 package kr.co.korearental.foregroundservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kr.co.korearental.foregroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            val intent = Intent(this, Foreground::class.java) //Foreground 클래스 전달
+            ContextCompat.startForegroundService(this, intent)
+        }
+
+        binding.buttonStop.setOnClickListener {
+            val intent = Intent(this, Foreground::class.java)
+            stopService(intent)
+        }
     }
 }
