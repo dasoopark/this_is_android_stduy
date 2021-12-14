2cac4e3c273683569a5b20c1b055aa37cbee923d
dasoopark <ektn0514@gmail.com>
2021-12-15 00:09:38 +0900

    .
    .

--- GoogleMaps/app/src/main/java/kr/co/korearental/googlemaps/MapsActivity.kt
+++ GoogleMaps/app/src/main/java/kr/co/korearental/googlemaps/MapsActivity.kt
@@ -1,7 +1,17 @@
 package kr.co.korearental.googlemaps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,34 +20,103 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import kr.co.korearental.googlemaps.databinding.ActivityMapsBinding
+
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+
     private lateinit var mMap: GoogleMap
+    val binding by lazy { ActivityMapsBinding.inflate(layoutInflater) }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(binding.root)
+
+        locationPermission = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            if(results.all{ it.value }) {
+                startProcess()
+            } else {
+                Toast.makeText(this
+                        , "권한 승인이 필요합니다."
+                        , Toast.LENGTH_LONG).show()
+            }
+        }
+
+        locationPermission.launch(
+                arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+        )
+    }
+
+    fun startProcess() {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        updateLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    @SuppressLint("MissingPermission")
+    fun updateLocation() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.run {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 1000
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult?.let {
+                    for(location in it.locations) {
+                        Log.d("Location", "${location.latitude} , ${location.longitude}")
+                        setLastLocation(location)
+                    }
+                }
+            }
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, //현재 위치 검색 API
+                Looper.myLooper())
+    }
+
+    fun setLastLocation(lastLocation: Location) {
+        val LATLNG = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+        // 커스텀 마커 사용하기
+        var bitmapDrawable: BitmapDrawable
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bitmapDrawable = getDrawable(R.drawable.marker) as BitmapDrawable
+        } else {
+            bitmapDrawable = resources.getDrawable(R.drawable.marker) as BitmapDrawable
+        }
+
+        var discriptor = BitmapDescriptorFactory.fromBitmap(bitmapDrawable.bitmap)
+
+        val markerOptions = MarkerOptions()
+                .position(LATLNG)
+                .title("Here!")
+                .icon(discriptor) // 커스텀 마커 적용
+
+        val cameraPosition = CameraPosition.Builder()
+                .target(LATLNG)
+                .zoom(15.0f)
+                .build()
+        mMap.clear()
+        mMap.addMarker(markerOptions)
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 }
