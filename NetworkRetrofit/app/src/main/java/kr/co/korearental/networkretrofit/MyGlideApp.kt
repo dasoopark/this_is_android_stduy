167d9e9b020cd69f002d9f97d7f6cb06b224d739
dasoopark <ektn0514@gmail.com>
2021-12-19 22:08:41 +0900

    .
    .

---
+++ NetworkRetrofit/app/src/main/java/kr/co/korearental/networkretrofit/MyGlideApp.kt
@@ -0,0 +1,8 @@
+package kr.co.korearental.networkretrofit
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyGlideApp : AppGlideModule() {
+}
---
+++ NetworkRetrofit/app/src/main/java/kr/co/korearental/networkretrofit/Repository.kt
@@ -0,0 +1,3 @@
+package kr.co.korearental.networkretrofit
+
+class Repository : ArrayList<RepositoryItem>()
