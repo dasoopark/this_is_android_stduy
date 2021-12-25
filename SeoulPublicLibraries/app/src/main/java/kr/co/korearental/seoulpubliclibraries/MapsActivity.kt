60ed81bbda67c9352ffb07b0ffb390cac63131b8
dasoopark <ektn0514@gmail.com>
2021-12-25 20:15:15 +0900

    .
    .

--- SeoulPublicLibraries/app/src/main/java/kr/co/korearental/seoulpubliclibraries/MapsActivity.kt
+++ SeoulPublicLibraries/app/src/main/java/kr/co/korearental/seoulpubliclibraries/MapsActivity.kt
@@ -13,31 +13,71 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        loadLibraries()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    fun loadLibraries() {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(SeoulOpenApi.DOMAIN)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        val seoulOpenService = retrofit.create(SeoulOpenService::class.java)
+        seoulOpenService
+                .getLibrary(SeoulOpenApi.API_KEY)
+                .enqueue(object : Callback<Library> {
+                    override fun onFailure(call: Call<Library>, t: Throwable) {
+                        Toast.makeText(baseContext
+                                , "서버에서 데이터를 가져올 수 없습니다."
+                                , Toast.LENGTH_LONG).show()
+                    }
+                    override fun onResponse(call: Call<Library>, response: Response<Library>) {
+                        showLibraries(response.body() as Library)
+                    }
+                })
+    }
+
+    fun showLibraries(libraries:Library) {
+        val latLngBounds = LatLngBounds.Builder()
+        for (lib in libraries.SeoulPublicLibraryInfo.row) {
+            val position = LatLng(lib.XCNTS.toDouble(), lib.YDNTS.toDouble())
+            val marker = MarkerOptions().position(position).title(lib.LBRRY_NAME)
+
+            var obj = mMap.addMarker(marker)
+            obj.tag = lib.HMPG_URL
+
+            mMap.setOnMarkerClickListener {
+                if (it.tag != null) {
+                    var url = it.tag as String
+                    if (!url.startsWith("http")) {
+                        url = "http://${url}"
+                    }
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                }
+                true
+            }
+
+            latLngBounds.include(marker.position)
+        }
+        val bounds = latLngBounds.build()
+        val padding = 0
+        val updated = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+        mMap.moveCamera(updated)
     }
 }
+
